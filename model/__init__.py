b3506a61b777679de8e995299751f3e778709d60
TakanariShimbo <takanari.shimbo@gmail.com>
2023-12-14 23:50:51 +0900

    add model of ChatGptSender

--- model/__init__.py
+++ model/__init__.py
@@ -1,2 +1,3 @@
 from .base import ColumnConfig, BaseEntity, BaseTable
 from .chat_gpt_model import ChatGptModelTable, ChatGptModelEntity
+from .chat_gpt_sender import ChatGptSenderTable, ChatGptSenderEntity
---
+++ model/chat_gpt_sender/table.py
@@ -0,0 +1,24 @@
+from typing import List, Type
+
+import pandas as pd
+
+from .entity import ChatGptSenderEntity
+from .. import ColumnConfig, BaseTable
+
+
+class ChatGptSenderTable(BaseTable[ChatGptSenderEntity]):
+    @staticmethod
+    def get_filepath() -> str:
+        return "./model/chat_gpt_model/data.csv"
+
+    @staticmethod
+    def get_column_config_list() -> List[ColumnConfig]:
+        return [
+            ColumnConfig(name="key", dtype=pd.StringDtype(), unique=True, non_null=True),
+            ColumnConfig(name="label_en", dtype=pd.StringDtype(), unique=True, non_null=True),
+            ColumnConfig(name="label_jp", dtype=pd.StringDtype(), unique=True, non_null=True),
+        ]
+    
+    @staticmethod
+    def get_entiry_class() -> Type[ChatGptSenderEntity]:
+        return ChatGptSenderEntity
