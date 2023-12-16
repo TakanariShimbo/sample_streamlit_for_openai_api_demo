5da76f7357eb18bd60432bb5a5cebbbe37931bf1
TakanariShimbo <takanari.shimbo@gmail.com>
2023-12-16 19:26:07 +0900

    revise base processers

--- app/base/processers.py
+++ app/base/processers.py
@@ -1,16 +1,57 @@
 from abc import ABC, abstractmethod
+from queue import Queue, Empty
 from threading import Thread
-from typing import List, Type, Dict, Any
+from typing import List, Type, Dict, Any, Generic, TypeVar, Optional
 
 
-class BaseProcesser(Thread, ABC):
-    def __init__(self) -> None:
+T = TypeVar("T")
+
+
+class QueueResponse(Generic[T]):
+    def __init__(self, content: T, is_finish: bool = False) -> None:
+        self._content = content
+        self._is_finish = is_finish
+
+    @property
+    def content(self) -> T:
+        return self._content
+
+    @property
+    def is_finish(self) -> bool:
+        return self._is_finish
+
+    def __str__(self) -> str:
+        return str(self._content)
+
+
+class QueueHandler(Generic[T]):
+    def __init__(self, timeout_sec: float = 1) -> None:
+        self._queue = Queue()
+        self._timeout_sec = timeout_sec
+
+    def send(self, content: T, is_finish: bool = False) -> None:
+        response = QueueResponse[T](content, is_finish)
+        self._queue.put(response)
+
+    def receive(self) -> Optional[QueueResponse[T]]:
+        try:
+            return self._queue.get(timeout=self._timeout_sec)
+        except Empty:
+            return None
+
+
+class BaseProcesser(Generic[T], Thread, ABC):
+    def __init__(self, timeout_sec: float = 1.0) -> None:
         super().__init__()
+        self._queue_handler = QueueHandler[T](timeout_sec)
         self._has_kwargs = False
 
     @property
     def kwargs(self) -> Dict[str, Any]:
         return self._kwargs
+    
+    def add_queue(self, content: T, is_finish: bool = False):
+        self._queue_handler.send(content=content, is_finish=is_finish)
 
     def start_and_wait_to_complete(self, **kwargs) -> None:
         if not self._has_kwargs:
@@ -24,6 +65,15 @@ class BaseProcesser(Thread, ABC):
         except RuntimeError:
             pass
         finally:
+            while True:
+                if not self.is_alive():
+                    break
+                response = self._queue_handler.receive()
+                if response is None:
+                    continue
+                self.callback_process(response.content)
+                if response.is_finish:
+                    break
             self.join()
 
         self.post_process(**self._kwargs)
@@ -41,6 +91,10 @@ class BaseProcesser(Thread, ABC):
 
     @abstractmethod
     def post_process(self, **kwargs) -> None:
+        raise NotImplementedError("Subclasses must implement this method")
+    
+    @abstractmethod
+    def callback_process(self, content: T, **kwargs) -> None:
         raise NotImplementedError("Subclasses must implement this method")
 
 
