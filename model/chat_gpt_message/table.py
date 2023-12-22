0f6b0d80238dbf172a5bd851601951f448893b9a
TakanariShimbo <takanari.shimbo@gmail.com>
2023-12-22 23:27:54 +0900

    revise model

---
+++ model/chat_gpt_message/table.py
@@ -0,0 +1,20 @@
+from typing import List, Type
+
+import pandas as pd
+
+from .entity import ChatGptMessageEntity
+from .. import ColumnConfig, BaseTable
+
+
+class ChatGptMessageTable(BaseTable[ChatGptMessageEntity]):
+    @staticmethod
+    def get_column_config_list() -> List[ColumnConfig]:
+        return [
+            ColumnConfig(name="role", dtype=pd.StringDtype(), unique=True, non_null=True, readonly=False),
+            ColumnConfig(name="name", dtype=pd.StringDtype(), unique=True, non_null=True, readonly=False),
+            ColumnConfig(name="content", dtype=pd.StringDtype(), unique=True, non_null=True, readonly=False),
+        ]
+
+    @staticmethod
+    def get_entiry_class() -> Type[ChatGptMessageEntity]:
+        return ChatGptMessageEntity
