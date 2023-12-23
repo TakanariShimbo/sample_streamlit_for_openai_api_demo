9bbb085b1d19a15c62c9779d0d6573bdde6499b3
TakanariShimbo <takanari.shimbo@gmail.com>
2023-12-23 15:35:29 +0900

    revise model

--- model/base/table.py
+++ model/base/table.py
@@ -12,49 +12,49 @@ T = TypeVar("T", bound="BaseTable")
 
 
 class BaseTable(Generic[E], ABC):
-    def __init__(self, table: pd.DataFrame) -> None:
-        self._validate(table)
-        self._table = table
+    def __init__(self, df: pd.DataFrame) -> None:
+        self._validate(df)
+        self._df = df
 
     @property
-    def table(self) -> pd.DataFrame:
-        return self._table
+    def df(self) -> pd.DataFrame:
+        return self._df
 
     @classmethod
     def append_b_to_a(cls: Type[T], table_a: T, table_b: T) -> T:
-        table = pd.concat([table_a.table, table_b.table], ignore_index=True)
-        return cls(table)
+        df = pd.concat([table_a.df, table_b.df], ignore_index=True)
+        return cls(df)
 
     @classmethod
     def load_from_entity_list(cls: Type[T], entity_list: List[E]) -> T:
         entity_data_list = [entity.to_dict() for entity in entity_list]
         dtype_dict = {config.name: config.dtype for config in cls.get_column_config_list()}
-        table = pd.DataFrame(entity_data_list).astype(dtype_dict)
-        return cls(table)
+        df = pd.DataFrame(entity_data_list).astype(dtype_dict)
+        return cls(df)
 
     @classmethod
     def load_from_csv(cls: Type[T], filepath: str) -> T:
         dtype_dict = {config.name: config.dtype for config in cls.get_column_config_list()}
-        table = pd.read_csv(filepath, dtype=dtype_dict)
-        return cls(table)
+        df = pd.read_csv(filepath, dtype=dtype_dict)
+        return cls(df)
 
     @classmethod
     def create_empty_table(cls: Type[T]) -> T:
         series_dict = {config.name: pd.Series(dtype=config.dtype) for config in cls.get_column_config_list()}
-        table = pd.DataFrame(series_dict)
-        return cls(table)
+        df = pd.DataFrame(series_dict)
+        return cls(df)
 
     def get_all_entities(self) -> List[E]:
-        return [self.get_entiry_class().init_from_series(series=row) for _, row in self._table.iterrows()]
+        return [self.get_entiry_class().init_from_series(series=row) for _, row in self._df.iterrows()]
 
     def get_entity(self, column_name: str, value: Any) -> E:
-        mask = self._table.loc[:, column_name] == value
-        matching_entities = self._table.loc[mask, :]
-        if matching_entities.empty:
+        rows_mask = self._df.loc[:, column_name] == value
+        matching_df = self._df.loc[rows_mask, :]
+        if matching_df.empty:
             raise ValueError(f"No rows found for {column_name}={value}")
-        elif matching_entities.shape[0] > 1:
+        elif matching_df.shape[0] > 1:
             raise ValueError(f"Multiple rows found for {column_name}={value}")
-        return self.get_entiry_class().init_from_series(series=matching_entities.iloc[0])
+        return self.get_entiry_class().init_from_series(series=matching_df.iloc[0])
 
     def _validate(self, df: pd.DataFrame) -> None:
         for config in self.get_column_config_list():
