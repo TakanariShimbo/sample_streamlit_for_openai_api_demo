d3e199b24d81d5236e893b67f09fe09872fa443f
TakanariShimbo <takanari.shimbo@gmail.com>
2023-12-24 19:22:39 +0900

    apply docker

---
+++ app_server/model/env.py
@@ -0,0 +1,7 @@
+import os
+
+from dotenv import load_dotenv
+load_dotenv()
+
+
+DEFAULT_OPENAI_API_KEY = os.environ["DEFAULT_OPENAI_API_KEY"]
--- model/env.py
+++
@@ -1,7 +0,0 @@
-import os
-
-from dotenv import load_dotenv
-load_dotenv()
-
-
-DEFAULT_OPENAI_API_KEY = os.environ["DEFAULT_OPENAI_API_KEY"]
